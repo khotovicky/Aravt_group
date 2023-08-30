eae0c147e6cd343c3bc41f0b960c449e01ed312f
khotovicky <yoddddu@gmail.com>
2023-08-30 14:07:58 +0300

    Вынес js бокового меню, уведомлений и формы заявки в отдельные сервисы

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
-$(document).ready(function() {
-    $('.nav-burger').click(() => {
-        $('.overlay, .nav-list').addClass('active')
-    })
-    $('.nav-list div, .overlay').click(() => {
-        $('.overlay, .nav-list').removeClass('active')
-    })
-})
-
-const inputs = document.querySelectorAll('input')
-const textarea = document.querySelector('textarea')
-const submitButton = document.querySelector('form p button')
-const acceptField = document.querySelector('.accept')
-const wrapper = document.querySelector('.wrapper')
 const overlay = document.querySelector('.overlay')
 
-
-submitButton.onclick = function() {
-    console.log(inputs);
-    let emptyField = 0
-    for (let input of inputs) {
-        console.log(input.value);
-        if (input.value == '') {
-            input.classList.add('request')
-            input.parentElement.classList.add('request')
-            emptyField++
-            } else {
-                input.classList.remove('request')
-                input.parentElement.classList.remove('request')
-            }
-    }
-    if (emptyField == 0) {
-        acceptField.style.display = 'flex'
-        for (let input of inputs) {
-            input.value = ''
-        }
-        textarea.value = ''
-        overlay.classList.add('active')
-    }
-}
-
-const acceptButton = document.querySelector('.accept-button')
-acceptButton.onclick = function() {
-    acceptField.style.display = 'none'
-    overlay.classList.remove('active')
-}
+const { openSideNav, closeSideNav } = initSideNavService(overlay)
+const { showNotification, hideNotification } = initNotificationsService(overlay)
+initOrderFormService(showNotification)
