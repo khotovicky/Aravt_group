7d5c39df0b598694f0dfcee66a1b10e497ec0123
khotovicky <yoddddu@gmail.com>
2023-09-05 12:04:06 +0300

    Повышена доступность отправки формы(отправка формы по событиям формы, в не по событиям кнопки)

--- orderForm.js
+++ orderForm.js
@@ -1,7 +1,7 @@
 function initOrderFormService(showNotification) {
     const orderForm = document.querySelector('#order-form')
     const fields = Array.from(orderForm.querySelectorAll('.field'))
-    const submitButton = orderForm.querySelector('#order-form p button')
+    // const submitButton = orderForm.querySelector('#order-form p button')
 
     function clearUnfilledMarks() {
         for (let field of fields) {
@@ -28,7 +28,9 @@ function initOrderFormService(showNotification) {
         return fields.filter(field => field.value === '' && field.hasAttribute('required'))
     }
     
-    submitButton.onclick = function() {
+    orderForm.onsubmit = function(event) {
+        event.preventDefault()
+
         const emptyRequiredFields = getEmptyRequiredFields(fields)
         markFieldsAsUnfilled(emptyRequiredFields)
         if (emptyRequiredFields.length === 0) {
@@ -37,9 +39,4 @@ function initOrderFormService(showNotification) {
             showNotification('Ваша заявка успешно оформлена!')
         }
     }
-
-    return {
-
-    }
-    
 }
